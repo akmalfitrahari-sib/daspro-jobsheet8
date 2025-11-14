0eb7db8aa9fd4a07681be17927188584411deeae
akmalfitrahari-sib <akmal.fitrahari@gmail.com>
2025-11-14 16:14:02 +0700

    While for v1

---
+++ NilaiKelompok03.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class NilaiKelompok03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i, j, nilai;
+        float totalNilai; 
+        float rataNilai;
+        
+        i = 1;
+        while (i <= 6) {
+            System.out.println("Kelompok " + i);
+
+            totalNilai = 0;
+            for(j=1; j<=5; j++){
+                System.out.print(" Nilai dari Kelompok Penilai " + j + ": ");
+                nilai = sc.nextInt();
+                totalNilai += nilai;
+            }
+
+            rataNilai = totalNilai/5;
+            System.out.println("Kelompok " + i + ": " + "Nilai rata-rata = " + rataNilai);
+            i++;
+            
+        }
+    }
+}
